b97efbda51a3f9a1ad4e576e377aa92e704d41e3
zhor1xc <name@example.ru>
2024-01-29 11:04:20 +0300

    Разработал программу на условия, которая должна проверять значение температуры

---
+++ task-2/main.js
@@ -0,0 +1,9 @@
+const temperature = 22;
+
+if(temperature > 30){
+    console.log('Вы можете надеть легкую одежду!');
+} else if (temperature > 20){
+    console.log('Вам нужно надеть теплую одежу!')
+} else if (temperature < 10){
+    console.log('Сегодня холодно, Вам нужно надеть зимнею одежду!')
+}
