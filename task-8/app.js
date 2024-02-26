222e573c97e13619c035c6fd32fe1b8fc9c9c279
zhor1xc <name@example.ru>
2024-02-26 11:11:11 +0300

    Выполнил практическое задание. Вариант 15

---
+++ task-8/app.js
@@ -0,0 +1,10 @@
+let number = (i) => {
+        if (Number.isInteger(result)) {
+            console.log(true);
+        } else {
+            console.log(false);
+    }
+}
+let key = prompt("Введите число");
+let result = parseFloat(key);
+number(result);
